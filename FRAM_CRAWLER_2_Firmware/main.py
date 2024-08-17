1703ec3af1d801b5a3043a1151a1ea01b7e8fb1f
Hanon <midohani25@gmail.com>
2024-08-17 01:58:19 -0400

    Simple bug

--- FRAM_CRAWLER_2_Firmware/main.py
+++ FRAM_CRAWLER_2_Firmware/main.py
@@ -4,7 +4,7 @@ FW_REV = 1.0
 FW_Release_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 def main ():
     print("DFT inspection system")
-    print ("FW Rev: ", FW_REV)
-    print("Release FW data and time: " ,FW_Release_date)
+    print("FW Rev: ", FW_REV)
+    print("Release FW date and time: " ,FW_Release_date)
 if __name__ == "__main__":
     main()
