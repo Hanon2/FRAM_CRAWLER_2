ed63b6644465dcb223b89c26b4a7e16979043486
Hanon <midohani25@gmail.com>
2024-08-22 21:22:51 -0400

    Simple updates to the rules document

--- FRAM_CRAWLER_2_Firmware/MSG_Handler.py
+++ FRAM_CRAWLER_2_Firmware/MSG_Handler.py
@@ -1,9 +1,18 @@
+#This file is for handling the messages
+# It will take the data that the Pi recieved from the Control box
+# It will parse these messages, and then tell the program what to do
+#It will also handle sending the messages to the control box
+
+requestToConnect_CMD = 0xAA
+takeAMeasurement_CMD = 0x55
+photoReceived_ACK = 0xD5
+positionRecieved_ACK = 0x2A
 def parseMessages(message):
-    if message == '0xAA':
+    if message == requestToConnect_CMD:
         print ("Handle the Request to connect")
-    elif message == '0x55':
+    elif message == takeAMeasurement_CMD:
         print("Handle the take a measurement")
-    elif message == '0xD5':
-        print("Photo recieved")
+    elif message == photoReceived_ACK:
+        print("Photo received")
     else:
-        print("Unkown data recieved")
+        print("Unknown data received")
--- FRAM_CRAWLER_2_Firmware/main.py
+++ FRAM_CRAWLER_2_Firmware/main.py
@@ -1,14 +1,9 @@
-from datetime import datetime
 import serial
-import binascii as b
 import MSG_Handler
 FW_REV = 1.0
-# Get the current date and time
-FW_Release_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 def main ():
     print("DFT inspection system")
     print("FW Rev: ", FW_REV)
-    print("Release FW date and time: " ,FW_Release_date)
     # Configure the serial connection
     ser = serial.Serial(
         port='COM3',  # Replace with the correct COM port
@@ -18,6 +13,6 @@ def main ():
     while True:
         data = ser.read(1)
         if data:
-            MSG_Handler.parseMessages(b.hexlify(data))
+            MSG_Handler.parseMessages(data)
 if __name__ == "__main__":
     main()
